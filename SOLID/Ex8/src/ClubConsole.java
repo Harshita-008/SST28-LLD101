8753458c94cf13118c4eee22e7c2674dc977bde5
Harshita-008 <harshitahirawat08@gmail.com>
2026-03-02 10:04:36 +0530

    Answer:8

--- SOLID/Ex8/src/ClubConsole.java
+++ SOLID/Ex8/src/ClubConsole.java
@@ -8,9 +8,9 @@ public class ClubConsole {
     }
 
     public void run() {
-        ClubAdminTools treasurer = new TreasurerTool(ledger);
-        ClubAdminTools secretary = new SecretaryTool(minutes);
-        ClubAdminTools lead = new EventLeadTool(events);
+        IFinanceOperations treasurer = new TreasurerTool(ledger);
+        IMinutesOperations secretary = new SecretaryTool(minutes);
+        IEventOperations lead = new EventLeadTool(events);
 
         treasurer.addIncome(5000, "sponsor");
         secretary.addMinutes("Meeting at 5pm");
---
+++ SOLID/Ex8/src/IMinutesOperations.java
@@ -0,0 +1,3 @@
+public interface IMinutesOperations {
+    void addMinutes(String text);
+}
